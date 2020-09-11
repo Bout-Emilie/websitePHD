b98d865d18b31ff3b4cec1c3bd243db7ee7b702c
BOUT-EMILIE <bout.emilie1@gmail.com>
2020-09-11 09:37:37 +0200

    first2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,35 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<div class="content m-12">
+<div class="intro ">
+	
+<div class="photo inline h-250 inline  float-left w-200 mr-6 mb-2"><h1 class="mb-5 capitalize  text-xl text-red-800">About : </h1><img alt='Me' src='me.jpg' class=""></div>
+<div class="text-intro mt-8 ml-5 w-3/5 inline leading-7">
+<p class="inline"> <br>I am Ph.D student working  under the supervison of <a href="http://researchers.lille.inria.fr/~loscri/personal-injury.html" class="underline text-gray-900"> Valeria Loscrí</a>
+	and <a href="https://antoine-gallais.github.io" class="underline text-gray-900">Antoine Gallais</a> in Future Ubiquitous Networks <a href="https://team.inria.fr/fun">(FUN)</a> team at Inria Lille Nord Europe. </p>
+<h2 class="mt-8 text-lg text-red-800 mb-3">Ph.D Topic:</h2>
+<div>
+ <span class="title text-gray-700 tracking-wide">Denial-of-Sleep on IoT networks</span>
+ <div class="para-1">The subject of my thesis is on attacks that can lead to denial-of-sleep on IoT networks. Nowadays IoT devices still have some constraints such as being limited in energy. 
+	Attacks leading to Denial-of-sleep are based on this vulnerability. Their goal consists of forcing the victim node to waste its energy in order to put them out of service. At a time when IoT devices are more and more present in our lives and in critical infrastructures, it becomes essential to study this type of attack.
+ </div>
+ <div class="para-2">
+	To better understand the vulnerabilities that exist on IoT networks my first objective is to put myself in the attacker's shoes in order to create attacks that can lead to a denial of sleep. We have focused for the moment on jamming attacks. We are also studying the possibility of creating intelligent attacks using machine learning in order to make these latter more efficient and more targeted. In the second time, my next objective will be to create solutions against this type of attack. 
+	The final goal of this thesis is to make the communications protocols more robust as well as the detection systems against sleep denial attacks.
+	<p class=""> This ph.D is a grant of <a href="https://www.defense.gouv.fr/dga">DGA. </a> </p>
+ </div>
+ 
+</div>
+</div>
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+</div>
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+<h2 class="mt-8 text-lg text-red-800 mb-3">Research Interests:</h2>
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+<ul class="list-disc ml-5">
+	<li>Internet of Things</li>
+	<li>Wireless sensor networks</li>
+	<li>MAC protocols, Routing Protocols for Wireless Networks</li>
+	<li>Security</li>
+	<li>Machine learning</li>
+</ul>
 
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+</div>
