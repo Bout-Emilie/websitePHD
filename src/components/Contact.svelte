6c750ece4ad37ba48c9625b37e4d388ecd77aa04
BOUT-EMILIE <bout.emilie1@gmail.com>
2020-09-14 18:18:03 +0200

    add scholar

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -11,7 +11,7 @@
     <div class="institue no-underline hover:underline text-red-800 text-lg"> <a href={WEBSITEINSTITURE}>{INSTITUTE}</a></div>
     <div class="email ">emilie.bout@inria.fr</div>
     <div class="logo flex space-x-4 ">
-        <div class="w-1/5"><a href=""><img alt='Google Scholar' src='google.png' class=""></a></div>
+        <div class="w-1/5"><a href="https://scholar.google.fr/citations?user=K9mjGDUAAAAJ&hl=fr&oi=ao"><img alt='Google Scholar' src='google.png' class=""></a></div>
         <div class="w-1/6 mt-1"><a href="https://www.linkedin.com/in/emilie-bout-050a68117/?originalSubdomain=fr"><img alt='Linkedin' src='linkedin.png' class=""></a></div>
         
     </div>
