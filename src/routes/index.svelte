67448cf877bbc94a9854ca08118542c40108d2e5
BOUT-EMILIE <bout.emilie1@gmail.com>
2022-02-10 11:09:08 +0100

    home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,17 +8,20 @@
 <div class="photo inline h-1/3 inline  float-left w-1/3 mr-6 mb-2"><h1 class="mb-5 capitalize  text-xl text-red-800">About : </h1><img alt='Me' src='me2.jpg' class=""></div>
 <div class="text-intro mt-8 ml-5 w-3/5 inline leading-7">
 <p class="inline"> <br>I am Ph.D student working  under the supervison of <a href="http://researchers.lille.inria.fr/~loscri/personal-injury.html" class="underline text-gray-900"> Valeria Loscrí</a>
-	and <a href="https://antoine-gallais.github.io" class="underline text-gray-900">Antoine Gallais</a> in Future Ubiquitous Networks <a href="https://team.inria.fr/fun">(FUN)</a> team at Inria Lille Nord Europe. </p>
+	and <a href="https://antoine-gallais.github.io" class="underline text-gray-900">Antoine Gallais</a> in Future Ubiquitous Networks <a href="https://team.inria.fr/fun">(FUN)</a> team at Inria Lille Nord Europe. This thesis is founded by the <a href="https://www.defense.gouv.fr/dga" class="underline">General Armament
+		Direction, France (DGA) </a> </p> 
 <h2 class="mt-8 text-lg text-red-800 mb-3">Ph.D Topic:</h2>
 <div>
- <span class="title text-gray-700 tracking-wide">Denial-of-Sleep on IoT networks</span>
- <div class="para-1">The subject of my thesis is on attacks that can lead to denial-of-sleep on IoT networks. Nowadays IoT devices still have some constraints such as being limited in energy. 
-	Attacks leading to Denial-of-sleep are based on this vulnerability. Their goal consists of forcing the victim node to waste its energy in order to put them out of service. At a time when IoT devices are more and more present in our lives and in critical infrastructures, it becomes essential to study this type of attack.
- </div>
- <div class="para-2">
-	To better understand the vulnerabilities that exist on IoT networks my first objective is to put myself in the attacker's shoes in order to create attacks that can lead to a denial of sleep. We have focused for the moment on jamming attacks. We are also studying the possibility of creating intelligent attacks using machine learning in order to make these latter more efficient and more targeted. In the second time, my next objective will be to create solutions against this type of attack. 
-	The final goal of this thesis is to make the communications protocols more robust as well as the detection systems against sleep denial attacks.
-	<p class=""> This ph.D is a grant of <a href="https://www.defense.gouv.fr/dga">DGA. </a> </p>
+ <span class="title text-gray-700 tracking-wide">Denial-of-Sleep Attacks on IoT networks</span>
+ <div class="para-1">The Internet of Things (IoT) has continued gaining in popularity and importance in everyday life in recent years. However, due to the number of sensitive and private data produced
+	by IoT systems, IoT networks becomes the new privileged targets for cyberattackers. In addition, this devices are constraints in ressources and energy. Therefore, due to their characteristics, security systems developed for IoT networks are an open question in research. 
+	At the same time, Machine Learning (ML) has gained a phenomenal success in various fields like telecommunications, transport or cybersecurity. Nonetheless, the application of ML can cause significant damage when put in the hands of an attacker. 
+	The increasing use of machine learning in creating attacks is significantly changing the threat landscape in two notable ways. The first will be the expansion of existing attacks. Indeed, by integrating ML algorithms in current cyberattacks, these will become more resistant, more reactive, and less recognizable by the existing
+	detection methods. The second will represent the creation of new threats which, until then, were not achievable due to their massive demand for data or their excessive manual processing time. </div>
+ <div class="para-2 mt-4">
+	To better understand the vulnerabilities that exist on IoT networks, my first objective is to take the place of an attacker. We focus on the creation of smart jamming attack. 
+	Indeed, this simple attack can have serious consequences ranging from the loss of data to the decommissioning of an IOT device. Few solutions exist to counter smart jamming attacks. 
+	Therefore, my first objective is to create smart jammming attacks in order to identify the flaws in the defense systems and then improve them.
  </div>
  
 </div>
@@ -29,10 +32,9 @@
 <h2 class="mt-8 text-lg text-red-800 mb-3">Research Interests:</h2>
 
 <ul class="list-disc ml-5">
+	<li>Cyber-Security</li>
 	<li>Internet of Things</li>
 	<li>Wireless sensor networks</li>
-	<li>MAC protocols, Routing Protocols for Wireless Networks</li>
-	<li>Security</li>
 	<li>Machine learning</li>
 </ul>
 
